46fbd73a785e6360d0de34a6681a5d191446a9e6
Maxim Hallasaar <endermax200113@mail.ru>
2023-10-03 08:33:17 +0300

    Added 'TextLine' and 'Line' components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import './assets/sass/App.sass';
-import ArticleBlock from './component/UI/article/ArticleBlock';
-import { TypeArticle } from './util/article';
+import Line from './component/UI/line/Line';
 
 function App() {
 	return (
 		<div className="app">
 			<div className="component">
-				{/*  */}
-				<ArticleBlock
-					category="Категория 1"
-					date="2020-12-13"
-					title="Управление ИТ-активами – скучная рутина или творческая задача?"
-					img={require("./assets/img/article1.png")}
-					type={TypeArticle.ONLY_TITLE}
-				>
-					Размышляя об управлении ИТ-активами, я вспомнил один учебный пример. Менеджер по ИТ-мощностям в крупной компании периодически готовил толстенный отчёт руководству. В очередной раз он не принёс отчёт, решив проверить, нужен ли он вообще. 
-				</ArticleBlock>
+				<Line />
 			</div>
 		</div>
 	);
