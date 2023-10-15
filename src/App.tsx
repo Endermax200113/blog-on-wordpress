0d3e53421221c1a075844f519038f101f3f9c671
Maxim Hallasaar <endermax200113@mail.ru>
2023-10-15 12:40:07 +0300

    Fixed a bug due to which when hovering and activating a normal button there was no response

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './assets/sass/App.sass';
-import Sidebar from './component/sidebar/Sidebar';
 
 function App(): React.ReactElement {
 	return (
 		<div className="app">
 			<div className="component">
-				<Sidebar />
+				
 			</div>
 		</div>
 	);
